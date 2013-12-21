55a1f19133bb592e0635926ba7243f0d186e0bf5
dsitum <dsitum@foi.hr>
2013-12-21 03:27:56 +0100

    dodao log u PocetnaActivity na kraj metode onCreate

--- mKino/src/hr/air/mkino/PocetnaActivity.java
+++ mKino/src/hr/air/mkino/PocetnaActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -63,6 +64,8 @@ public class PocetnaActivity extends Activity {
 				
 			}
 		}); 
+        
+        Log.d("AIR", "Kreirano");
     }
 
 
